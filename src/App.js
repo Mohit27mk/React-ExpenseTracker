c60202f8ec1754b2d8c890a6e14866a60f44b29d
Mohit27mk <114071726+Mohit27mk@users.noreply.github.com>
2023-04-27 06:23:40 +0530

    expense component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import ExpenseItem from "./components/ExpenseItem";
 
+const expenses=[
+  {
+    id:1,
+   date:new Date(2021,2,28),
+   title:"Car Insurance",
+   amount:294.67,
+   location:"Shahad"
+  },
+  {
+    id:2,
+    date:new Date(2022,4,8),
+    title:"Bike Insurance",
+    amount:29.67,
+    location:"Shahad"
+   },
+   {
+    id:3,
+    date:new Date(2024,11,2),
+    title:"Food",
+    amount:94.67,
+    location:"Kalyan"
+   },
+   {
+    id:4,
+    date:new Date(2021,2,28),
+    title:"Dmart",
+    amount:294.67,
+    location:"maharal"
+   },
+  
+  ]
 
 function App() {
   return (
     <div>
-      <h1>
-        Lets Get started
-        <ExpenseItem></ExpenseItem>
-      </h1>
+      <h2>Lets get started</h2>
+       {
+        expenses.map((value)=>(
+        <ExpenseItem title={value.title} date={value.date} location={value.location} amount={value.amount} ></ExpenseItem>
+        ))
+       }
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,15 @@
 import './ExpenseItem.css';
 
-function ExpenseItem(){
-const expenseDate=new Date(2021,2,28);
-const expenseTitle="Car Insurance";
-const expenseAmount=294.67;
-const expenseLocation="Shahad";
+function ExpenseItem(props){
 
+console.log(props);
     return(
       <div className='expense-item'>
-        <div>{expenseDate.toDateString()}</div>
+        <div>{props.date.toDateString()}</div>
        <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <h3>{expenseLocation}</h3>
-        <div className='expense-item__price'>{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <h3>{props.location}</h3>
+        <div className='expense-item__price'>{props.amount}</div>
        </div>
        </div>
     );
