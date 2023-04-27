29f6134d923c588253600ca231650d0ceec8c4ce
Mohit27mk <114071726+Mohit27mk@users.noreply.github.com>
2023-04-27 05:57:05 +0530

    expenseItem component added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,20 @@
+import './ExpenseItem.css';
+
 function ExpenseItem(){
+const expenseDate=new Date(2021,2,28);
+const expenseTitle="Car Insurance";
+const expenseAmount=294.67;
+const expenseLocation="Shahad";
+
     return(
-            <div>
-        <h2>Expense Items</h2>
-        <p>Food Rs 10</p>
-        <p>Petrol Rs 109</p>
-        <p>Movies Rs 200</p>
-            </div>
+      <div className='expense-item'>
+        <div>{expenseDate.toDateString()}</div>
+       <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <h3>{expenseLocation}</h3>
+        <div className='expense-item__price'>{expenseAmount}</div>
+       </div>
+       </div>
     );
 }
 
