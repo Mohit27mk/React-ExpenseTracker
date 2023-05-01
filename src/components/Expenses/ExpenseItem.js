26935dafc8bc1b8536da6005dd8ccef37ffc01bd
Mohit27mk <114071726+Mohit27mk@users.noreply.github.com>
2023-05-02 00:08:32 +0530

    adding button and event listeners

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,12 +4,16 @@ import ExpenseDetails from './ExpenseDetails';
 import Card from '../UI/Card';
 
 function ExpenseItem(props){
+const clickHandler=()=>{
 
+}
 
     return(
       <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
+     <button onClick={clickHandler}>Change Title</button>
+    <button>Delete Expense</button>
     </Card>
     );
 }
