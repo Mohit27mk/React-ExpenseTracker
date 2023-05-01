5665670f388019216feb1f670ff4a73c279d8e40
Mohit27mk <114071726+Mohit27mk@users.noreply.github.com>
2023-05-02 00:52:27 +0530

    by using usestate change title dinamically

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,18 +2,27 @@ import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import Card from '../UI/Card';
+import { useState } from 'react';
 
 function ExpenseItem(props){
-const clickHandler=()=>{
 
+  const [title,setTitle]=useState(props.title);
+  const [amount,setAmount]=useState(props.amount);
+
+const changeTitle=()=>{
+setTitle('updated Title');
+}
+
+const changePrice=()=>{
+  setAmount(100);
 }
 
     return(
       <Card className='expense-item'>
       <ExpenseDate date={props.date} />
-      <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
-     <button onClick={clickHandler}>Change Title</button>
-    <button>Delete Expense</button>
+      <ExpenseDetails amount={amount}  location={props.location}   title={title} />
+     <button onClick={changeTitle}>Change Title</button>
+    <button onClick={changePrice}>update price</button>
     </Card>
     );
 }
